79cca99be511db1d9704228593b4b3e6738423bf
Ros <traine41@icloud.com>
2023-08-25 13:48:25 +0100

    added missing fields

--- src/main/java/spring/boot/bookstore/model/Book.java
+++ src/main/java/spring/boot/bookstore/model/Book.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Entity
+@Table(name = "books")
 @Data
 public class Book {
     @Id
@@ -18,6 +20,8 @@ public class Book {
     private String title;
     @Column(nullable = false)
     private String author;
+    @Column(nullable = false,unique = true)
+    private String isbn;
     @Column(nullable = false)
     private BigDecimal price;
     private String description;
