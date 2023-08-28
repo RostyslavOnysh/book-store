fbd6decaa64c0992b7246be66dd5fe8b422f11d6
Ros <traine41@icloud.com>
2023-08-28 15:45:15 +0100

    modified BookService

--- src/main/java/spring/boot/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/spring/boot/bookstore/service/impl/BookServiceImpl.java
@@ -1,26 +1,40 @@
 package spring.boot.bookstore.service.impl;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import spring.boot.bookstore.dto.request.CreateBookRequestDto;
+import spring.boot.bookstore.dto.response.BookDto;
+import spring.boot.bookstore.exception.EntityNotFoundException;
+import spring.boot.bookstore.mapper.BookMapper;
 import spring.boot.bookstore.model.Book;
 import spring.boot.bookstore.repository.BookRepository;
 import spring.boot.bookstore.service.BookService;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Override
+    public BookDto createBook(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.createBook(book));
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public List<BookDto> findAll() {
+        return bookRepository.getAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto getBookById(Long id) {
+        Book book = bookRepository.getBookById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find books by id: " + id)
+        );
+        return bookMapper.toDto(book);
     }
 }
