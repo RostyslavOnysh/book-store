5af01d38aa3ba13e9ff96dd7593be80edde28c71
Ros <traine41@icloud.com>
2023-08-28 15:47:20 +0100

    modified other classes

--- src/main/java/spring/boot/bookstore/model/Book.java
+++ src/main/java/spring/boot/bookstore/model/Book.java
@@ -7,20 +7,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import spring.boot.bookstore.dto.response.BookDto;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "books")
-@Data
-public class Book {
+public class Book extends BookDto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
     @Column(nullable = false)
     private String title;
     @Column(nullable = false)
     private String author;
-    @Column(nullable = false,unique = true)
+    @Column(nullable = false, unique = true)
     private String isbn;
     @Column(nullable = false)
     private BigDecimal price;
