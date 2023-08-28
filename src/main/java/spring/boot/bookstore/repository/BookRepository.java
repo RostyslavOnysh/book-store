a0f446222e137d8c4e56915123ff08b29e70f98a
Ros <traine41@icloud.com>
2023-08-28 15:45:28 +0100

    modified BookRepository

--- src/main/java/spring/boot/bookstore/repository/BookRepository.java
+++ src/main/java/spring/boot/bookstore/repository/BookRepository.java
@@ -1,10 +1,13 @@
 package spring.boot.bookstore.repository;
 
 import java.util.List;
+import java.util.Optional;
 import spring.boot.bookstore.model.Book;
 
 public interface BookRepository {
-    Book save(Book book);
+    Book createBook(Book book);
 
-    List<Book> findAll();
+    Optional<Book> getBookById(Long id);
+
+    List<Book> getAll();
 }
--- src/main/java/spring/boot/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/spring/boot/bookstore/repository/impl/BookRepositoryImpl.java
@@ -1,51 +1,57 @@
 package spring.boot.bookstore.repository.impl;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.springframework.dao.DataAccessException;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
+import spring.boot.bookstore.exception.DataProcessingException;
+import spring.boot.bookstore.exception.EntityNotFoundException;
 import spring.boot.bookstore.model.Book;
 import spring.boot.bookstore.repository.BookRepository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
+    private final EntityManagerFactory entityManagerFactory;
 
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    public BookRepositoryImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     @Override
-    public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.save(book);
+    public Book createBook(Book book) {
+        EntityTransaction transaction = null;
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(book);
             transaction.commit();
             return book;
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataAccessException("Can't create book " + book, e) {
-            };
-        } finally {
-            if (session != null) {
-                session.close();
-            }
+            throw new DataProcessingException("Can't insert a book: " + book, e);
         }
     }
 
     @Override
-    public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session
-                    .createQuery("FROM Book", Book.class)
-                    .getResultList();
+    public Optional<Book> getBookById(Long id) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            Book book = entityManager.find(Book.class, id);
+            return Optional.ofNullable(book);
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Can't find books by id: " + id);
+        }
+    }
+
+    @Override
+    public List<Book> getAll() {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Can't find all books");
         }
     }
 }
