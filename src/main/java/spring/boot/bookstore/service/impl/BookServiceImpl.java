d4cd24b2711b87cb06d02e780a52b52d24ee12ef
Ros <traine41@icloud.com>
2023-08-31 14:52:13 +0100

    was added new methods

--- src/main/java/spring/boot/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/spring/boot/bookstore/service/impl/BookServiceImpl.java
@@ -1,8 +1,10 @@
 package spring.boot.bookstore.service.impl;
 
+import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import spring.boot.bookstore.dto.BookSearchParametersDto;
 import spring.boot.bookstore.dto.request.CreateBookRequestDto;
 import spring.boot.bookstore.dto.response.BookDto;
 import spring.boot.bookstore.exception.EntityNotFoundException;
@@ -20,21 +22,56 @@ public class BookServiceImpl implements BookService {
     @Override
     public BookDto createBook(CreateBookRequestDto requestDto) {
         Book book = bookMapper.toModel(requestDto);
-        return bookMapper.toDto(bookRepository.createBook(book));
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
     public List<BookDto> findAll() {
-        return bookRepository.getAll().stream()
+        return bookRepository.findAll().stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
 
     @Override
     public BookDto getBookById(Long id) {
-        Book book = bookRepository.getBookById(id).orElseThrow(
+        Book book = bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Can't find books by id: " + id)
         );
         return bookMapper.toDto(book);
+    }
+
+    @Override
+    public List<BookDto> getBookByTitle(String title) {
+        return bookRepository.findAllByTitleContainsIgnoreCase(title)
+                .stream()
+                .map(bookMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book by id: " + id));
+        book.setDeleted(true);
+        bookRepository.save(book);
+    }
+
+    @Override
+    public BookDto updateBook(Long id, CreateBookRequestDto bookRequestDto) {
+        Book updatedBook = bookMapper.toModel(bookRequestDto);
+        updatedBook.setId(id);
+        return bookMapper.toDto(bookRepository.save(updatedBook));
+    }
+
+    @Override
+    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters) {
+        List<Book> searchResults = bookRepository.searchBooks(
+                searchParameters.getTitle(),
+                searchParameters.getAuthor(),
+                searchParameters.getIsbn()
+        );
+        return searchResults.stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
