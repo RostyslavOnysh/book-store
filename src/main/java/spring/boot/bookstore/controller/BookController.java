ebf75ef0819c7b9816e5ae60f0da6bfdf6962d05
Ros <traine41@icloud.com>
2023-08-31 14:47:00 +0100

    I added four new endpoints. 1. getAllByTitle 2.delete 3.updateById 4.searchBooks

--- src/main/java/spring/boot/bookstore/controller/BookController.java
+++ src/main/java/spring/boot/bookstore/controller/BookController.java
@@ -2,12 +2,18 @@ package spring.boot.bookstore.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import spring.boot.bookstore.dto.BookSearchParametersDto;
 import spring.boot.bookstore.dto.request.CreateBookRequestDto;
 import spring.boot.bookstore.dto.response.BookDto;
 import spring.boot.bookstore.service.BookService;
@@ -31,5 +37,26 @@ public class BookController {
     @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
         return bookService.createBook(requestDto);
+    }
+
+    @GetMapping("/title")
+    public List<BookDto> getAllByTitle(@RequestParam String title) {
+        return bookService.getBookByTitle(title);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateById(@PathVariable Long id,
+                              @RequestBody CreateBookRequestDto bookRequestDto) {
+        return bookService.updateBook(id, bookRequestDto);
+    }
+    @GetMapping("/search")
+    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters){
+        return bookService.searchBooks(searchParameters);
     }
 }
