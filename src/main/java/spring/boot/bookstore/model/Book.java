b0b41dc812ecc9b9dd781073ce833f183f668983
Ros <traine41@icloud.com>
2023-08-31 14:45:00 +0100

    modified book entity using the SQL delete query

--- src/main/java/spring/boot/bookstore/model/Book.java
+++ src/main/java/spring/boot/bookstore/model/Book.java
@@ -7,11 +7,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import spring.boot.bookstore.dto.response.BookDto;
 
+@Data
 @Entity
+@SQLDelete(sql = "UPDATE books SET is_Deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Getter
 @Setter
 @Table(name = "books")
@@ -28,6 +34,10 @@ public class Book extends BookDto {
     private String isbn;
     @Column(nullable = false)
     private BigDecimal price;
+    @Column
     private String description;
+    @Column(name = "coverImage")
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
