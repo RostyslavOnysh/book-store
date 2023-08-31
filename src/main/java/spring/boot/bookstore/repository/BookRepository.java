26366c846b5fd72bc8fc1ffa208a2e1dc9a03363
Ros <traine41@icloud.com>
2023-08-31 14:47:25 +0100

    Modified Repository class

--- src/main/java/spring/boot/bookstore/repository/BookRepository.java
+++ src/main/java/spring/boot/bookstore/repository/BookRepository.java
@@ -1,13 +1,20 @@
 package spring.boot.bookstore.repository;
 
 import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spring.boot.bookstore.model.Book;
 
-public interface BookRepository {
-    Book createBook(Book book);
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findAllByTitleContainsIgnoreCase(String title);
 
-    Optional<Book> getBookById(Long id);
+    @Query("SELECT b FROM Book b WHERE "
+            + "(:title is null or lower(b.title) like %:title%) "
+            + "and (:author is null or lower(b.author) like %:author%) "
+            + "and (:isbn is null or b.isbn = :isbn)")
+    List<Book> searchBooks(@Param("title") String title,
+                           @Param("author") String author,
+                           @Param("isbn") String isbn);
 
-    List<Book> getAll();
 }
