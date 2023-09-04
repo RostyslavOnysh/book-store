e453f4e634e0271931b34e0b4bda35de1ce68424
Ros <traine41@icloud.com>
2023-09-04 15:19:16 +0100

    added new annotations

--- src/main/java/spring/boot/bookstore/dto/request/CreateBookRequestDto.java
+++ src/main/java/spring/boot/bookstore/dto/request/CreateBookRequestDto.java
@@ -1,14 +1,24 @@
 package spring.boot.bookstore.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import spring.boot.bookstore.validation.book.author.Author;
+import spring.boot.bookstore.validation.book.isbn.Isbn;
+import spring.boot.bookstore.validation.book.title.Title;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
+    @Title
     private String title;
+    @NotNull
+    @Author
     private String author;
+    @Isbn
     private String isbn;
-
+    @Min(0)
     private BigDecimal price;
 
     private String description;
