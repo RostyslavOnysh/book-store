e9ed1e0c7513d3423254b6ad7dcab173a4517264
Ros <traine41@icloud.com>
2023-09-11 14:52:48 +0100

    In book service implement Logger and modified 'updateBook' method

--- src/main/java/spring/boot/bookstore/service/book/impl/BookServiceImpl.java
+++ src/main/java/spring/boot/bookstore/service/book/impl/BookServiceImpl.java
@@ -1,35 +1,39 @@
-package spring.boot.bookstore.service.impl;
+package spring.boot.bookstore.service.book.impl;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-import spring.boot.bookstore.dto.BookSearchParameter;
-import spring.boot.bookstore.dto.request.CreateBookRequestDto;
-import spring.boot.bookstore.dto.response.BookDto;
+import spring.boot.bookstore.dto.book.BookResponseDto;
+import spring.boot.bookstore.dto.book.BookSearchParameter;
+import spring.boot.bookstore.dto.book.CreateBookRequestDto;
 import spring.boot.bookstore.exception.EntityNotFoundException;
 import spring.boot.bookstore.mapper.BookMapper;
 import spring.boot.bookstore.model.Book;
 import spring.boot.bookstore.repository.BookRepository;
 import spring.boot.bookstore.repository.specification.book.builders.BookSpecificationBuilder;
-import spring.boot.bookstore.service.BookService;
+import spring.boot.bookstore.service.book.BookService;
 
 @RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
+    private static final Logger logger = LogManager.getLogger(BookServiceImpl.class);
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
     private final BookSpecificationBuilder bookSpecificationBuilder;
 
     @Override
-    public BookDto createBook(CreateBookRequestDto requestDto) {
+    public BookResponseDto createBook(CreateBookRequestDto requestDto) {
+        logger.info("Creating a new book: {}", requestDto.getTitle());
         Book book = bookMapper.toModel(requestDto);
         return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public List<BookDto> findAll(Pageable pageable) {
+    public List<BookResponseDto> findAll(Pageable pageable) {
         return bookRepository.findAll(pageable)
                 .stream()
                 .map(bookMapper::toDto)
@@ -37,7 +41,7 @@ public class BookServiceImpl implements BookService {
     }
         
     @Override
-    public BookDto getBookById(Long id) {
+    public BookResponseDto getBookById(Long id) {
         Book book = bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Can't find books by id: " + id)
         );
@@ -45,7 +49,7 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> getBookByTitle(String title) {
+    public List<BookResponseDto> getBookByTitle(String title) {
         return bookRepository.findAllByTitleContainsIgnoreCase(title)
                 .stream()
                 .map(bookMapper::toDto)
@@ -61,14 +65,21 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public BookDto updateBook(Long id, CreateBookRequestDto bookRequestDto) {
-        Book updatedBook = bookMapper.toModel(bookRequestDto);
-        updatedBook.setId(id);
-        return bookMapper.toDto(bookRepository.save(updatedBook));
+    public BookResponseDto updateBook(Long id, CreateBookRequestDto bookRequestDto) {
+        Book bookById = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find books by id: " + id)
+        );
+        bookById.setTitle(bookRequestDto.getTitle());
+        bookById.setAuthor(bookRequestDto.getAuthor());
+        bookById.setIsbn(bookRequestDto.getIsbn());
+        bookById.setPrice(bookRequestDto.getPrice());
+        bookById.setDescription(bookRequestDto.getDescription());
+        bookById.setCoverImage(bookRequestDto.getCoverImage());
+        return bookMapper.toDto(bookRepository.save(bookById));
     }
 
     @Override
-    public List<BookDto> searchBooks(BookSearchParameter searchParameters) {
+    public List<BookResponseDto> searchBooks(BookSearchParameter searchParameters) {
         Specification<Book> bookSpecification = bookSpecificationBuilder.build(searchParameters);
         return bookRepository.findAll(bookSpecification)
                 .stream()
