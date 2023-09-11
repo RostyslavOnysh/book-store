7e0e61bd6b24c7469d374116fb4de3040b7ff4e2
Ros <traine41@icloud.com>
2023-09-11 14:51:52 +0100

    Was created BookSearchParameter

---
+++ src/main/java/spring/boot/bookstore/dto/book/BookSearchParameter.java
@@ -0,0 +1,5 @@
+package spring.boot.bookstore.dto.book;
+
+public record BookSearchParameter(String[] title, String[] author, String[] isbn,
+                                  String [] price, String description) {
+}
