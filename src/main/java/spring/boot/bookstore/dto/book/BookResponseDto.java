77be54fda0534dacfa6aade1902d418990f348fa
Ros <traine41@icloud.com>
2023-09-18 14:57:32 +0100

    added category filed

--- src/main/java/spring/boot/bookstore/dto/book/BookResponseDto.java
+++ src/main/java/spring/boot/bookstore/dto/book/BookResponseDto.java
@@ -1,6 +1,7 @@
 package spring.boot.bookstore.dto.book;
 
 import java.math.BigDecimal;
+import java.util.Set;
 import lombok.Data;
 
 @Data
@@ -12,4 +13,5 @@ public class BookResponseDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    private Set<Long> categoryIds;
 }
