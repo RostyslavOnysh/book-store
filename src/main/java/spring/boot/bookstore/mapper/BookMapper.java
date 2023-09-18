3ada323a897cb88e82297b709537f0e7dda66b04
Ros <traine41@icloud.com>
2023-09-18 14:54:13 +0100

    was added new method setCategoryIds

--- src/main/java/spring/boot/bookstore/mapper/BookMapper.java
+++ src/main/java/spring/boot/bookstore/mapper/BookMapper.java
@@ -1,14 +1,30 @@
 package spring.boot.bookstore.mapper;
 
+import java.util.stream.Collectors;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import spring.boot.bookstore.config.MapperConfig;
 import spring.boot.bookstore.dto.book.BookResponseDto;
+import spring.boot.bookstore.dto.book.BookWithoutCategoryIdsDto;
 import spring.boot.bookstore.dto.book.CreateBookRequestDto;
 import spring.boot.bookstore.model.Book;
+import spring.boot.bookstore.model.Category;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
+    @Mapping(target = "categoryIds", ignore = true)
     BookResponseDto toDto(Book book);
 
-    Book toModel(CreateBookRequestDto requestDto);
+    Book toModel(CreateBookRequestDto bookDto);
+
+    BookWithoutCategoryIdsDto toDtoWithoutCategories(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookResponseDto bookResponseDto, Book book) {
+        bookResponseDto.setCategoryIds(book.getCategories().stream()
+                .map(Category::getId)
+                .collect(Collectors.toSet()));
+    }
 }
