d82a058110a7c527db5e09c040c7645d29fffe87
Ros <traine41@icloud.com>
2023-10-04 12:43:45 +0100

    modified Mappings

--- src/main/java/spring/boot/bookstore/mapper/OrderMapper.java
+++ src/main/java/spring/boot/bookstore/mapper/OrderMapper.java
@@ -9,6 +9,8 @@ import spring.boot.bookstore.model.Order;
 
 @Mapper(config = MapperConfig.class, uses = OrderItemMapper.class)
 public interface OrderMapper {
-    @Mappings({@Mapping(target = "userId", source = "user.id")})
+    @Mappings({@Mapping(target = "userId", source = "user.id"),
+            @Mapping(source = "orderDate", target = "orderTime")})
+    @Mapping(source = "total", target = "totalPrice")
     OrderResponseDto toDto(Order order);
 }
