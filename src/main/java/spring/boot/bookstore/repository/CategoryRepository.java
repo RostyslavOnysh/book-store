437148e67e1b33c0db5707a0a5b7a77570239262
Ros <traine41@icloud.com>
2023-10-19 12:57:49 +0100

    new method to find a category by name was added

--- src/main/java/spring/boot/bookstore/repository/CategoryRepository.java
+++ src/main/java/spring/boot/bookstore/repository/CategoryRepository.java
@@ -6,5 +6,5 @@ import spring.boot.bookstore.model.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String fantasy);
 }
-
