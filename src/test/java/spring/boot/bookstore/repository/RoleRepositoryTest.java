83c58bb437ae604e4fc0da2ceac8b7dd81951973
Ros <traine41@icloud.com>
2023-10-24 16:54:56 +0100

    fix checkstyle

--- src/test/java/spring/boot/bookstore/repository/RoleRepositoryTest.java
+++ src/test/java/spring/boot/bookstore/repository/RoleRepositoryTest.java
@@ -3,7 +3,6 @@ package spring.boot.bookstore.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
